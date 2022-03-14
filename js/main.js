b3778707e456d6caafe05dcfafeb722d9460598b
microdotmatrix <liquid.knives.2@gmail.com>
2022-03-14 02:31:52 -0500

    dark mode

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-let parallax_sections = document.querySelectorAll('.hero__image');
-for (let parallax_section of parallax_sections) {
-	let _from = parallax_section.getAttribute("data-parallax-from") || '0px';
-	let _to = parallax_section.getAttribute("data-parallax-to") || '100px';
-	let instance = basicScroll.create({
-		elem: parallax_section,
-		from: 'top-middle',
-		to: 'bottom-middle',
-		direct: true,
-		props: {
-			'--ty': {
-				from: _from,
-				to: _to,
-			}
-		}
-	})
-	instance.start();
-}
+$(".theme-switch").on("click", () => {
+	$("body").toggleClass("light-mode");
+});
